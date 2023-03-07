c96b13742a7c0c575bd8bc0308fb56915e1ae119
ricardo <ricardo95balam@gmail.com>
2023-03-07 00:16:39 -0600

    first commit

---
+++ src/components/FeaturedWorks.js
@@ -0,0 +1,16 @@
+import SectionHeader from "./SectionHeader";
+import Work from "./Work";
+
+export default function FeaturedWorks() {
+  return (
+    <section className="px-6">
+      <div className="max-w-4xl mx-auto py-12">
+        <SectionHeader title="Featured work" href="#" />
+        <div className="flex flex-col gap-2">
+          <Work />
+          <Work />
+        </div>
+      </div>
+    </section>
+  )
+}
