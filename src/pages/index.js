253c2fb6a8ff8469561ce940681716f7f39b1baa
ricardo <ricardo95balam@gmail.com>
2023-03-16 20:27:09 -0600

    add more info

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import IntroHeader from "../components/IntroHeader";
 import RecentPosts from '../components/RecentPosts';
 import FeaturedWorks from '../components/FeaturedWorks';
 import { getAllPosts, getAllWork } from '../../lib/api';
-import Skills from '@/components/Skills';
-import SoftSkills from '@/components/softSkills';
+import Skills from '..components/Skills';
+import SoftSkills from '../components/SoftSkills';
 
 export async function getStaticProps() {
   const posts = getAllPosts();
