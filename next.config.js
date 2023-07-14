3ff03ad9e96b8fd2a412908157a035430a83de9b
ricardo <ricardo95balam@gmail.com>
2023-07-13 22:23:58 -0500

    add cloudinary images

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
         hostname: 'raw.githubusercontent.com',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+      }
     ],
   },
 }
