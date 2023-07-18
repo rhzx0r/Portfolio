c2e9c65a82c63fe8144a84138d98198205cf2347
ricardo <ricardo95balam@gmail.com>
2023-07-18 01:48:04 -0600

    add works logos

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,25 @@
 import Image from "next/image";
+import js from '../../public/logos/javascript.svg';
+import react from '../../public/logos/reactjs.svg'
+import bootstrap from '../../public/logos/bootstrap.svg'
+import api from '../../public/logos/api.svg'
+import openai from '../../public/logos/openai.svg'
 
 export default function Work({ item }) {
+
+  const logos = {
+    js: js,
+    react: react,
+    bootstrap: bootstrap,
+    api: api,
+    openai: openai
+  };
+
+  
+  function getLogo(logo) {
+    return logos[logo.toLowerCase()] || null;
+  }
+
   return (
     <article className="flex items-center border-b-2 py-2 flex-col max-h-full md:flex-row md:max-h-72 ">
       <Image
@@ -23,6 +42,21 @@ export default function Work({ item }) {
           </a>
         </div>
         <p>{item.description}</p>
+        <div className="flex mt-1 justify-end"> 
+        <div className="mr-1">Stack: </div> 
+        <a className="flex flex-row flex-wrap">
+          { item.stack &&
+            item.stack.map((logo) => (
+              <span
+                className="mr-1"
+                key={logo}
+              >
+                {" "}<Image src={getLogo(logo)} width={20} alt={logo} height={20} className="rounded-md" />
+              </span>
+            ))
+          }
+        </a>
+        </div>
       </div>
     </article>
   );
