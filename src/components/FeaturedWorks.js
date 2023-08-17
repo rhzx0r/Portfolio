808435960b0c5e881d0551d2e462432a3b1665ab
ricardo <ricardo95balam@gmail.com>
2023-08-16 21:13:52 -0600

    add popover

--- src/components/FeaturedWorks.js
+++ src/components/FeaturedWorks.js
@@ -3,9 +3,9 @@ import Work from "./Work";
 
 export default function FeaturedWorks({ work }) {
   return (
-    <section className="px-6 ">
+    <section className="md:px-6 ">
       <div className="max-w-4xl mx-auto py-12">
-        <div className="bg-white dark:bg-slate-700 shadow-md rounded-lg p-2 pt-4">
+        <div className="bg-white dark:bg-slate-700 shadow-md md:rounded-lg md:p-4 p-6 md:pt-6">
           <SectionHeader title="🛠️ Proyectos" href="#"/>
           <div className="flex flex-col gap-2">
             {work.map((workItem) => (
